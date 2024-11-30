4fd5a8281775124438024b36bdd3acee41686656
Phil Helm <git@phelm.co.uk>
2024-11-30 19:50:02 +0000

    feat: Add setup page with IngredientPicker component

---
+++ src/app/setup/page.tsx
@@ -0,0 +1,9 @@
+import IngredientPicker from '@/components/ingredient-picker'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white dark:bg-stone-950">
+      <IngredientPicker />
+    </main>
+  )
+}
