06639f8f3111a76b124eeb6627da22541f34b94c
Phil Helm <git@phelm.co.uk>
2024-12-02 21:06:04 +0000

    fix: Add missing imports and async effect in Step3 component

--- src/components/Step3.tsx
+++ src/components/Step3.tsx
@@ -1,3 +1,14 @@
+import { useEffect } from 'react'
+
 export default function Step3({ next }) {
+  useEffect(async () => {
+    const { text } = await getRecipe({
+      mealName: item,
+      protein: selectedProtein!,
+      carb: selectedCarb!,
+      veg: selectedVeg!,
+    })
+    setRecipeText(text)
+  })
   return <h1>Step 3</h1>
 }
