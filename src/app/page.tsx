28475d248a411aaeb13edf09867f4aacf964746b
Phil Helm <git@phelm.co.uk>
2024-12-03 10:51:11 +0000

    feat: Add max duration configuration for page rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Wizard from '@/components/Wizard'
 
+export const maxDuration = 30
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 md:p-24">
